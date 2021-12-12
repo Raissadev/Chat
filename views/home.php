f147ab334697e29ab4a73dd75aab59986d62e8a8
Raissadev <raissa.fullstack@gmail.com>
2021-12-12 11:52:45 -0300

    Update files

--- views/home.php
+++ views/home.php
@@ -1,7 +1,8 @@
 <?php require 'components/flash-message.php'; ?>
 
-<section class="container-welcome items-flex align-center just-center">
-    <div class="wrap w75 items-flex just-center box">
+<section class="container-welcome items-flex align-center just-center flex-wrap">
+    <a class="toggle hide-device-bigger w90 center"><i class="ri-menu-line"></i></a>
+    <div class="wrap w75 items-flex just-center box w90-device-small flex-wrap">
         <div class="col w50">
             <h1 class="font-size-bigger">Welcome <?= $_SESSION['name'] ?>!</h1>
             <div class="row margin-top-default">
